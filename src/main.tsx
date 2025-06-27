d625527c1a591c8ea3ea7518f47ba33eff2dd339
jam92444 <jamaltwo.3121@gmail.com>
2025-06-27 10:43:33 +0530

    added

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "./styles/main.scss";
+import "./Styles/main.scss";
 import { AuthProvider } from "./Context/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
